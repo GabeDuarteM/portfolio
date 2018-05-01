78c4b6e3f57686afbac3a21cafa3579efc65a049
Gabriel Duarte <gabrielqduarte@gmail.com>
2018-04-30 22:22:17 -0300

    feat(_document): removed the production script tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,36 +10,6 @@ class CustomDocument extends Document {
     return (
       <html lang="en">
         <Head>
-          {process.env.NODE_ENV === "production" && (
-            <React.Fragment>
-              <script
-                async
-                src="https://www.googletagmanager.com/gtag/js?id=UA-106120004-2"
-              />
-              <script
-                // eslint-disable-next-line react/no-danger
-                dangerouslySetInnerHTML={{
-                  __html: `window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', 'UA-106120004-2');`,
-                }}
-              />
-              <script
-                // eslint-disable-next-line react/no-danger
-                dangerouslySetInnerHTML={{
-                  __html: `(function(h,o,t,j,a,r){
-                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                    h._hjSettings={hjid:848161,hjsv:6};
-                    a=o.getElementsByTagName('head')[0];
-                    r=o.createElement('script');r.async=1;
-                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                    a.appendChild(r);
-                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
-                }}
-              />
-            </React.Fragment>
-          )}
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0"
