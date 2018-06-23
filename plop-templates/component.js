63695592d88119ee7ba33366551fc5d11b8b51c3
Gabriel Duarte <gabrielqduarte@gmail.com>
2018-06-23 20:59:22 -0300

    chore(plop): added plop templates

---
+++ plop-templates/component.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const {{pascalCase name}} = () => (
+  <div>
+    {{pascalCase name}}
+    <style jsx>{`
+      .{{dashCase name}}-root {
+      }
+    `}</style>
+  </div>
+)
+
+export default {{pascalCase name}}
