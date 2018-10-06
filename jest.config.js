0117085fb2d48a1b28eff8d776c87df571ab67b6
Gabriel Duarte <gabrielqduarte@gmail.com>
2018-10-06 03:14:01 -0300

    chore: update and remove unecessary deps

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  setupFiles: ["<rootDir>/jest.setup.js"],
-  testPathIgnorePatterns: [
-    "<rootDir>/src/.next/",
-    "<rootDir>/node_modules/",
-    "<rootDir>/src/out/",
-  ],
-}
